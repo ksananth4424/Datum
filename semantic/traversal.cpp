d33cd1739d2ee8e1edf752e01cd9d7ed6b50a1ae
Srikar-Arugonda-1 <cs22btech11008@iith.ac.in>
2024-11-06 17:20:09 +0530

    type check in case of array

--- semantic/traversal.cpp
+++ semantic/traversal.cpp
@@ -448,7 +448,7 @@ void traverse_declaration(DeclarationStatement *decl_stmt)
                     if(debug) {cout << "inserting into symbol table" << endl;}
                     symtab.insert(name, type, scope, decl_stmt->row, decl_stmt->column);
                 }
-                //  type checking here. example: int a = 5.1 should give error here
+                //  type checking here
                 if (init_decl->initializer != nullptr && init_decl->initializer->assignmentExpression!=nullptr && init_decl->initializer->assignmentExpression->expression!=nullptr){
                     Expression* expr = init_decl->initializer->assignmentExpression->expression;
                     DataType rhs = traverse_operations(expr,scope);
@@ -506,10 +506,72 @@ void traverse_declaration(DeclarationStatement *decl_stmt)
                     }
                 }
                 // type checking in case of array
-
-                } else cout<<"initilizer is null"<<endl;
+                else if (init_decl->initializer != nullptr && init_decl->initializer->initializerList != nullptr)
+                {
+                    for (auto *initializer : *(init_decl->initializer->initializerList))
+                    {
+                        if (initializer->assignmentExpression != nullptr && initializer->assignmentExpression->expression != nullptr)
+                        {
+                            Expression *expr = initializer->assignmentExpression->expression;
+                            DataType rhs = traverse_operations(expr,scope);
+                            if (type == Integer)
+                            {
+                                if (rhs != Integer && rhs != Boolean)
+                                {
+                                    std::cout << "\e[31m Error: \e[0m Cannot assign " << dataTypeToString(rhs) << " to integer\n";
+                                    printerror(initializer->row,initializer->column);
+                                    error_count++;
+                                }
+                            }
+                            else if (type == Float)
+                            {
+                                if (rhs != Float && rhs != Integer && rhs != Boolean)
+                                {
+                                    std::cout << "\e[31m Error: \e[0m Cannot assign " << dataTypeToString(rhs) << " to float\n";
+                                    printerror(initializer->row,initializer->column);
+                                    error_count++;
+                                }
+                            }
+                            else if (type == Boolean)
+                            {
+                                if (rhs != Boolean && rhs != Integer)
+                                {
+                                    std::cout << "\e[31m Error: \e[0m Cannot assign " << dataTypeToString(rhs) << " to boolean\n";
+                                    printerror(initializer->row,initializer->column);
+                                    error_count++;
+                                }
+                            }
+                            else if (type == String && rhs != String)
+                            {
+                                std::cout << "\e[31m Error: \e[0m Cannot assign " << dataTypeToString(rhs) << " to string\n";
+                                printerror(initializer->row,initializer->column);
+                                error_count++;
+                            }
+                            else if (type == Char && rhs != Char)
+                            {
+                                std::cout << "\e[31m Error: \e[0m Cannot assign " << dataTypeToString(rhs) << " to char\n";
+                                printerror(initializer->row,initializer->column);
+                                error_count++;
+                            }
+                            else if (type == Dataset && rhs != Dataset)
+                            {
+                                std::cout << "\e[31m Error: \e[0m Cannot assign " << dataTypeToString(rhs) << " to dataset\n";
+                                printerror(initializer->row,initializer->column);
+                                error_count++;
+                            }
+                            else if (type == Array && rhs != Array)
+                            {
+                                std::cout << "\e[31m Error: \e[0m Cannot assign " << dataTypeToString(rhs) << " to array\n";
+                                printerror(initializer->row,initializer->column);
+                                error_count++;
+                            }
+                        }
+                    }
+                } 
+                else cout<<"initilizer is null"<<endl;
             }
         }
+    }
     if(debug) {cout << "exiting declaration" << endl;}
 }
 
